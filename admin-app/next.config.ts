84ce83b2eae147ce263b3643b2cb66bd0fe4dd25
AvishkarKedar <AvishkarKedar@users.noreply.github.com>
2026-08-26 19:38:48 +0530

    chore: prepared admin-app for Vercel deployment

--- admin-app/next.config.ts
+++ admin-app/next.config.ts
@@ -1,18 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Deployed to Render, so full-stack API routes are enabled!
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    // Helps reduce memory usage during builds
-    memoryBasedWorkersCount: true,
-  },
-  output: 'standalone',
 };
 
 export default nextConfig;
